08638eac45e3531a219dd06f5be42abbe903f38b
Tobiasz Fic <tobi.fic@gmail.com>
2023-11-18 16:49:27 +0100

    Add argument parsing for fitting

--- src/fit.cpp
+++ src/fit.cpp
@@ -1,10 +1,37 @@
 #include "GAUSPIDFit2D.hpp"
 
-int main()
+static inline bool check_argparse(const char* arg, const std::string long_form, const std::string short_form)
 {
-    auto filelist_path = "filelist_train.txt";
-    auto out_path = "gauss_out.root";
+    auto res1 = strcmp(arg, long_form.c_str());
+    auto res2 = strcmp(arg, short_form.c_str());
+    return (res1 == 0) || (res2 == 0);
+}
+
+int main(int argc, char** argv)
+{
+    std::string filelist_path = "filelist_train.txt";
+    std::string out_path = "gauss_out.root";
     int nbins = 50;
+
+    using namespace std;
+    for(int i = 1; i < argc; ++i)
+    {
+        if(check_argparse(argv[i], "--filelist", "-f"))
+        {
+            filelist_path = std::string(argv[++i]);
+            cout << "Input filelist path: " << filelist_path << endl;
+        }
+        if(check_argparse(argv[i], "--output", "-o"))
+        {
+            out_path = std::string(argv[++i]);
+            cout << "Output file path: " << out_path << endl;
+        }
+        if(check_argparse(argv[i], "--nbins", "-nb"))
+        {
+            nbins = atoi(argv[++i]);
+            cout << "Number of bins: " << nbins << endl;
+        }
+    }
 
     const float p_min = 0;
     const float p_max = 6;
@@ -33,7 +60,7 @@ int main()
         fit.ConcatenateFits();
     }
 
-    TFile* out_file = TFile::Open(out_path, "recreate");
+    TFile* out_file = TFile::Open(out_path.c_str(), "recreate");
 
     std::cout << "Writing to file..." << std::endl;
     for(auto& fit: fits)
